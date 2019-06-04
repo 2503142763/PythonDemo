bc11fd6d88b577bdd4c83000415d6cea2a1b45a2
2503142763 <2503142763@qq.com>
2019-06-04 08:33:29 +0800

    oneA.py
    辅函数

---
+++ oneA.py
@@ -0,0 +1,16 @@
+import sys
+import socket
+import machine
+import time
+import network
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+  print('connecting to network...')
+  sta_if.active(True)
+  sta_if.connect('黎', '15678658862')
+  #sta_if.connect('nubia', 'yellowlmf1234')
+  while not sta_if.isconnected():
+    pass
+print('network config:', sta_if.ifconfig())
+
+
