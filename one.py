aacb337c31bf4f2922051a751078fee736ab6001
2503142763 <2503142763@qq.com>
2019-06-04 08:38:02 +0800

    Update one.py

--- one.py
+++ one.py
@@ -2,12 +2,3 @@ import ujson
 import time
 import MQTT
 import machine
-
-print('***************adding from pythonA')
-print('***************adding from pythonA***************')
-print('***************adding from pythonA***************')
-print('***************adding from pythonA****************')
-print('***************adding from pythonA***************')
-print('***************adding from pythonA***************')
-print('***************adding from pythonA***************')
-print('***************adding from pythonA***************')
